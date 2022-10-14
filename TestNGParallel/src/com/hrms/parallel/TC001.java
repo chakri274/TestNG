f63bc6d3afb6aea421420a65720b5aed41b2195e
chakri274 <chakradharcap7@gmail.com>
2022-10-14 14:33:48 +0530

    Chakri done this Testng project

---
+++ TestNGParallel/src/com/hrms/parallel/TC001.java
@@ -0,0 +1,48 @@
+package com.hrms.parallel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+
+public class TC001 {
+	WebDriver driver;
+	@Test
+	public void openFF() throws Exception {
+		
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		Reporter.log("HRMS OPENED");
+		Thread.sleep(4000);
+		
+		}
+	
+	@Parameters({"browser"})
+	
+	@BeforeMethod
+	public void setUp(String browser) {
+		
+		if(browser.equals("Firefox")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\\\Softwares\\\\Drivers\\\\geckodriver-v0.30.0-win64\\\\geckodriver.exe");
+			driver= new FirefoxDriver();
+		}
+		else if(browser.equals("Chrome")) {
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		    driver = new ChromeDriver();
+		    
+		}
+		
+	}
+	@AfterMethod
+	public void closeFF() {
+		driver.close();
+	}
+
+}
